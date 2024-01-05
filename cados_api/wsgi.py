763193fa53498020de6e2f2e143003aee9f91a5a
Big Steppa <steppacodes@gmail.com>
2024-01-05 18:08:00 +0100

    project setup

---
+++ cados_api/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for cados_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from decouple import config
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'cados_api.settings.{config("SETTINGS")}')
+
+application = get_wsgi_application()
